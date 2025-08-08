d97e967d03c2e272aa850ed1d8f005651035682e
Ryan Hell <ryan_hell@outlook.com>
2025-08-08 02:15:34 -0700

    Update BagShawGallery.js

--- components/BagShawGallery.js
+++ components/BagShawGallery.js
@@ -55,7 +55,8 @@ export default function Gallery() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-8">Image Gallery</h1>
+      <h1 className="text-3xl font-bold text-center mb-8">"Where is Ryan? I Cant let Ryan see me!"</h1>
+    <h2>"It doesnt matter you have to go now the deputies are waiting at the end of the road they wont come on the property while you are here, you have to go now!"</h2>
       
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {images.map((image, index) => (
