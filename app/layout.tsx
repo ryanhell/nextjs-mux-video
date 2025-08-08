d9bfae119cb4e31ac1432b37549461f77acf923a
Ryan Hell <ryan_hell@outlook.com>
2025-08-08 02:33:07 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,14 @@ export default function RootLayout({
         <main className="flex-1   overflow-y-auto">
           <div className="p-6  px-8  flex-1 ">
             <Header />
-            <div className="">{children}
+            <div className="">
+              <h1>Chelan County Superior Court Case 2410025304</h1>
+              <h3>3 Counts of Felony Harassment</h3>
+              <p>Deep state kidnapping and attemopted murder foiled and they are charging me with completely bogus alligations after they denied us ambulance for declared life threatening medical emrgenciues, verified as life in danger by one of them; instead of ambulance they took us to an iolated desert canyon road to a home where our cell ophones would not work. They had unknown men go steal my truck and drive it to this house where they began demanding a cash ransom and assaulting me, denied us phone calls for over 2 hours; threatened to shoot me, would not let us leave or talk to each other threatenmed to shoot me. Called a chelan county sheriff deputy buddy and ordered me to be disappeared for good. They have tried to hide evidence. Tampered with video and denied me a digital forenics preservation motion. </p>
+              <p>Sometime towards the end of this ordeal a woman was ordered to flee the scene so deputies could come onto the property. </p>
+
+              <p>Ive since identified this woman as Sally Bagshaw, a formre CHIEF PROSECUTIOR in King County and a Seattle City Council Woman who was unusually protective of Ed Murray the disgraced and resigned SERIAL pedophile mayor who prayed on black CPS kids he was trusted with/</p>
+              {children}
               <BagShawGallery />
             </div>
           </div>
