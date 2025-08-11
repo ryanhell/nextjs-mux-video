11292e5765798ebc19637112b842b37d5da870ad
Ryan Hell <ryan_hell@outlook.com>
2025-08-11 02:45:48 -0700

    pdf image well

--- components/CAD1.tsx
+++ components/CAD1.tsx
@@ -2,16 +2,23 @@ import Image from 'next/image'
 
 export default function CAD1() {
     return (
-        <><Image
-            src="/CADFiles/intialDuressCallto911/24C06391 _page1.jpg"
-            width={500}
-            height={500}
-            alt="Picture of the author" />
-            <Image
-                src="/CADFiles/intialDuressCallto911/RyanHell391_page2.jpg"
-                width={500}
-                height={500}
-                alt="Picture of the author" />
+        <>
+            <div className="flex flex-col md:flex-row">
+                <div className="w-full md:w-1/2 p-4 bg-blue-200">
+                    <Image
+                        src="/CADFiles/intialDuressCallto911/24C06391 _page1.jpg"
+                        width={500}
+                        height={500}
+                        alt="Picture of the author" />
+                </div>
+                <div className="w-full md:w-1/2 p-4 bg-green-200">
+                    <Image
+                        src="/CADFiles/intialDuressCallto911/RyanHell391_page2.jpg"
+                        width={500}
+                        height={500}
+                        alt="Picture of the author" />
+                </div>
+            </div>
         </>
     )
 }
