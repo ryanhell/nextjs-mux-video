b31bc4420be08f60e5a4212ddd71bd19772640da
Ryan Hell <ryan_hell@outlook.com>
2025-08-11 02:22:18 -0700

    CAD 1 jpg add

--- components/CAD1.tsx
+++ components/CAD1.tsx
@@ -2,11 +2,16 @@ import Image from 'next/image'
 
 export default function CAD1() {
     return (
-        <Image
+        <><Image
             src="/CADFiles/intialDuressCallto911/24C06391 _page1.jpg"
             width={500}
             height={500}
-            alt="Picture of the author"
-        />
+            alt="Picture of the author" />
+            <Image
+                src="/CADFiles/intialDuressCallto911/RyanHell391_page2.jpg"
+                width={500}
+                height={500}
+                alt="Picture of the author" />
+        </>
     )
 }
