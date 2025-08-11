5e2dea38cdb985c5f887c2fe633c0c5a25728405
Ryan Hell <ryan_hell@outlook.com>
2025-08-11 02:49:54 -0700

    pdf image well

--- components/CAD1.tsx
+++ components/CAD1.tsx
@@ -4,11 +4,11 @@ export default function CAD1() {
     return (
         <>
             <div className="flex flex-col md:flex-row">
-                <div className="w-full md:w-1/2 p-4 bg-blue-200">
+                <div className="w-full md:w-1/2 p-4 bg-blue-20 relative">
                     <Image
                         src="/CADFiles/intialDuressCallto911/24C06391 _page1.jpg"
-                        width={500}
-                        height={500}
+                        layout="fill"
+                        objectFit="cover"
                         alt="Picture of the author" />
                 </div>
                 <div className="w-full md:w-1/2 p-4 bg-green-200">
