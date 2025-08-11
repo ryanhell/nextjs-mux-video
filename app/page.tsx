44750d758a9289a0472632c35e58994af6704fd3
Ryan Hell <ryan_hell@outlook.com>
2025-08-11 01:13:49 -0700

    CAD 1 jpg add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Background from "@/components/Background";
 import Image from "next/image";
+import CAD1 from "@/components/CAD1";
 
 export default function Home() {
   return (
     <div className="py-6">
       <Background />
+      <CAD1 />
     </div>
   );
 }
---
+++ components/CAD1.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function CAD1() {
+    return (
+        <Image
+            src="/CADFiles/intialDuressCallto911/24C06391 _page1.jpg"
+            width={500}
+            height={500}
+            alt="Picture of the author"
+        />
+    )
+}
