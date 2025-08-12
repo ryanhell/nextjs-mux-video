51c8b582df21beb6b62ed446a5fb6a9528970298
Ryan Hell <ryan_hell@outlook.com>
2025-08-12 15:52:02 -0700

    efefe3fe

--- app/aboutRyan/page.tsx
+++ app/aboutRyan/page.tsx
@@ -23,8 +23,11 @@ const page = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12 md:py-16 lg:py-20">
                 <div className="grid grid-cols-1 lg:grid-cols-1 gap-8 lg:gap-12">
                     <div className="lg:col-span-2 space-y-8">
-                        <h4 className="text-3xl text-slate-900">...Reformed convict, with a new lease on life after 5 yerars of engineering university reviews from authentic new customers.</h4>
-                        <h4 className="text-slate-900 TypographyH3">Crime Free Since 2002! That includes driving infractions and late library books. </h4>
+                        <h4 className="text-3xl text-slate-900">Orphan....Reformed convict, driven to help others... and genuinely a real volunteer. Unlike the claims of Rivercom and co who claimed to be good samaritans, FORCED us into riding to the Operations Manager house then ambushed me with a CASH demand...and thats IN their video evidence. Apparently they never knew they cant expect anything nor can yoy charge money in any way if you designate as a volunteer or good samaritan. The crazy part is when I reminded the Chelan Deputy that he seemed stunneed as if he didnt know that. </h4>
+
+                        <h4 className="text-3xl text-slate-900"> I'm a helpful man and a strong willed survicor who is not scared to stand up for those who cannot. I got a new lease on life after 5 yerars of engineering university reviews from authentic new customers.</h4>
+                        <h4 className="text-slate-900 text-2xl TypographyH3">100% Crime Free Since 2002!</h4>
+                        <p className="text-white"> That includes driving infractions and late library books! </p>
                         <h4 className="text-3xl text-slate-900">...3 time business owner and senior construction manager working with countless of townships, buros, and other larger size municioalities and cities across the east coast..dealing with dificult and frustrating problems and very angry and sometimes abusive even violent extortionist even.... its a thing out east too.</h4>
                         <h3 className="text-4xl text-slate-900">...Cum Laud Alumni from an accredited University Program 5 years of learning teams for software engineering, technical communications and business informatics.</h3>
                         <p className="text-slate-900 text-4xl">
