480896ea0a6aff981c502cef5ed114270257be5f
Ryan Hell <ryan_hell@outlook.com>
2025-08-12 17:16:14 -0700

    efefe3fe

--- app/aboutRyan/page.tsx
+++ app/aboutRyan/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { TypographyH3 } from "@/components/ui/TypographyH3";
+import { Card, CardContent, CardTitle, CardHeader } from "@/components/ui/card";
+import TypographyH3 from "@/components/ui/TypographyH3";
 
 const page = () => {
     return (
@@ -26,6 +27,31 @@ const page = () => {
                         <h4 className="text-3xl text-slate-900">Orphan....Reformed convict, driven to help others... and genuinely a real volunteer. Unlike the claims of Rivercom and co who claimed to be good samaritans, FORCED us into riding to the Operations Manager house then ambushed me with a CASH demand...and thats IN their video evidence. Apparently they never knew they cant expect anything nor can yoy charge money in any way if you designate as a volunteer or good samaritan. The crazy part is when I reminded the Chelan Deputy that he seemed stunneed as if he didnt know that. </h4>
 
                         <h4 className="text-3xl text-slate-900"> I'm a helpful man and a strong willed survicor who is not scared to stand up for those who cannot. I got a new lease on life after 5 yerars of engineering university reviews from authentic new customers.</h4>
+                        <Card>
+                            <CardHeader>
+                                <CardTitle>Volunteer Highlights</CardTitle>
+                            </CardHeader>
+                            <CardContent>
+                                <ol className="text-2xl">
+                                    <li>
+                                        35 mile winter logout clearing Fallen Old growth timber on Section J of the Pacific Crest Trail alongside Washington Trail Association `Backcountry Response Team`.
+                                    </li>
+                                    <li>
+                                        2 month volunteer carpentry gig helping out with remote renovations of historic Holden Villiage mining camp in North Cascades
+                                    </li>
+                                    <li>
+                                        <p className="underline decoration-2">Pandemic</p> <p>senior citizen emergency food distribution with Michael Kors `Gods Love We Deliver` located in Tribeca</p>
+                                    </li>
+                                    <li>
+                                        Delivered food by foot to many of the 3 million terrified seniors during the pandemic, helping Meals on Wheels of Manhattan NYC
+                                    </li>
+                                </ol>
+                            </CardContent>
+
+                        </Card>
+                        <div className="Card">
+
+                        </div>
                         <h4 className="text-slate-900 text-2xl TypographyH3">100% Crime Free Since 2002!</h4>
                         <p className="text-white"> That includes driving infractions and late library books! </p>
                         <h4 className="text-3xl text-slate-900">...3 time business owner and senior construction manager working with countless of townships, buros, and other larger size municioalities and cities across the east coast..dealing with dificult and frustrating problems and very angry and sometimes abusive even violent extortionist even.... its a thing out east too.</h4>
--- components/ui/TypographyH3.tsx
+++ components/ui/TypographyH3.tsx
@@ -1,4 +1,4 @@
-export function TypographyH3() {
+export default function TypographyH3() {
     return (
         <h3 className="scroll-m-20 text-6xl text-red-600 font-semibold tracking-tight">
             The Joke Tax
