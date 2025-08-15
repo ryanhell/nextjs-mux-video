b0cc7271d148028f15a5f195483bce3a3e0f5c28
Ryan Hell <ryan_hell@outlook.com>
2025-08-15 00:35:15 -0700

    wdwdw

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,44 @@
 import Background from "@/components/Background";
 import Image from "next/image";
 import CAD1 from "@/components/CAD1";
+import { Card, CardHeader, CardContent, CardTitle, CardDescription } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div className="py-6">
+      <h2 className="text-2xl">Dificult complex case with many twist....designed to confuse...by a 20 year senior CHIEF prosecutor {`(`}retired but still pulling shit apparently{`)`}</h2>
+      <h3 className="text-4xl">This incident was not just sketchy dispatchers and tow mutts trying to get over on tourist. </h3>
+      <p className="text-2xl">It was kidnapping with a clear conspiracy to commit murder while interfering in life threatening medical duress call to 911. I have not violated any laws or rules or even decency expectations of any sort. </p>
+      <p className="text-3xl">The sheer volume of discrepencioes and unaddressed detaails like what the hell was a retired former 20 year CHIEF prosecutor from Seattke doing runninj out the back door so to speak, of these scumbag corrupt city worjkers home?</p>
+
       <Background />
       <CAD1 />
+      <Card className="bg-black text-white">
+        <CardHeader>
+          <h2 className="text-3xl">Major Prosecutorial MISCONDUCT</h2>
+          <h3 className="text-2xl">Discrepencies...not just a few minor frivial things either.</h3>
+        </CardHeader>
+        <CardContent>
+          <ul>
+            <p className="text-2xl">WRONG TOW COMPANY INTENTIONALLY LISTED </p>
+            <p className="text-lg">After a lot of round and round and citing law and explaining I knew what they were doinf; saying they can take my truck but they will be arrested by STATE PATROL because Jeremiah interjected bragging how he had the local deputies in his phone on speed dial and he would call them for me; I said no worries, I will call State Patrol. And I may call 4 or 5 troops to get an honest one who will intervene. Or I might need to contact attorney general, to which he also laughed. I saud then that I would call FBI but not a day will go by that I stop seeking justice and bringing awaremess to these acts. Jeremiah became visible scared and the tow operator angrily threw my keys swearing at Jeremiah, saying he wants nothing to do with this. Smart man.Jeremiah howeever, for all the brains and glory he represents as an operations managaer at a large private dispatch center; wouldnt budge, picked the keys up and claimed he was TAKING my truck its his and I need to leave or I would be shot. Again. They refused to let my injured passenger even talk to me, and i woouldnt leave her. Being that the tow operator shifted down adn left his stake in this; I realized Jeremiah was the one who was supposed to shoot me if I refused to leave his property. Under vehicle abandonment law in Washington State, given the fact the B&T tow truck was there still; walking away could also cost me my vehicle. Jermeiahs wife Jenna ran over and punched me in the face at one point, without warning while Jeremiah was talking to me. They started spitting and hurling insults so I recognized their effort to provoke me into a fight for justifiable homicide had picked up considerably.  </p>
+            <p className="text-lg">The arresting deputies incident report as well as the 911 CAD file claim Stans Mount View Towing is the tow operator. Im not aware of any of that and the man who DROVE my truck without permission, tried to demand cash then backed out of it smartly; was driving this red roll-back with door tags indicating B&T Towing out of Cashmere Washington. Jeremiah and himself had claimed to be buddies. There was definately some bad blood after this mess, and it is apparent that he refused to cooperate with this now out of control and completely desperate criminal activity. They had to get some substance for their completely false narrative about me threatening these so called "heros" over some repair or tow bill. They simply resorted to a local tow company who they can confide in and this speaks volumes of the Stans Mountain View Towing. Obviously worth being cautious of if you need a tow in Chelan since I can assure you there was no tow of any sort nor was their any consent to tow, nor legal auithority nor did I need a tow. Nor did I need a tire. And I will check but I am pretty sure tow companies are tow companies. And they probably cant legitmately just offer to sell you a used tire and wheel even if that were what was requested. It cwertainly was not.</p>
+            <p className="text-2xl">Court is not allowing access to the actual victim / witness, i.e. the tow operator in this photo who was the only one there</p>
+            <p className="text-xl">Stans accepts credit cards, also. And would have gladly yielded me a bill upon request as they are obligated to do. Again, Stan Mountain View ior whoever is being falsely introduced to compensate for a uncooperative tow operator who knew he was in over his head and tapped out. My compliments to him for his albeit late moment of clarity. Now if we could contact him... I cannot I have a court order not to and assigned counsel has refused to do so. So by slight of hand they railroad me here also.</p>
+            <Image src="/Chelan/july4_2024/towtruck.jpg" width="1400" height="2800" alt="This is a photp of the tow and operator at the scene. Stans Mountain View or whatever never existed"></Image>
+            <div className="block mt-2 text-xl align-left antialiased font-normal leading-normal text-inherit mb-6">This is a screenshot from one of the prosecutions own videos.... it clearly shows B7T not Stans. Probably just trying to bog me down since I am fighting my case alone.</div>
+            <Image src="/Chelan/july4_2024/E6EF22E9-DB1F-49B3-87E1-28D2CDE1CFC6_4_5005_c.jpeg" width="1900" height="2800" alt="from left, in this photo: Jeremiah Johnson who is the Rivercom 911 OPERATIONS CENTER MANAGER, the actual tow operator, the female dispatcher they names as Jenna and now last minute have claimed is not and never has been a Rivercom employee... , unknown quiet man wearing wenatchee fire shirt, and possibly the other tow operator who had followed first tow operator as he drove my truck from the park to Jeremiahs home without my knowledge or consent, following him in the red B7T tow truck with a side by side ATV on bed. The two dogs are my passengers medical dogs and she never parts with them. They played on the element of causinf fear for my passengers safety by coddling her dogs and refusing to get her or say if she is ok, then laughing"></Image>
+
+            <div className="block mt-2 text-m align-left antialiased font-normal leading-normal text-inherit">
+              From left: Jeremiah Johnson the Rivercom 911 OPERATIONS CENTER MANAGER, tow operator on scene in my pickup, female dispatcher Jenna, unnamed quiet man wearing wenatchee fire shirt, and the 2nd tow operator who had followed first tow operator driving my truck to Jeremiahs home. The two dogs are my passengers medical dogs and she never parts with them.
+            </div>
+
+            <h3 className="text-2xl">Taunting Me, or Throwing Me a Bone?"</h3>
+            <p className="text-1xl">After nearly an entire YEAR, I was finally told I could watch but not take home Jenas videos. I was invited to assigned counsels office to watch a copy of un-tampered video which clearly showed deputy asking tow operatopr for impound bill and the tow operator said Ryan doesnt owe me anything. He was asked about impound, tow, and the services / parts rendered to "fix" my truck and the tow operator adimently and sternly informed the deputy that he was not owed ANYTHING by me and he had no interest in my truck. Yet that deputy lied on his incident report stating that I was outraged over some repair or impound bill I wasnt able to pay. Keep in mind also; my truck was given to my passenger to drive home. They wrote that up as "we dropped his truck off at the park and ride" but in fact the tow operator drove my passenger to the park and ride then sent her on her way. The deputy actually came to the car window to get my consent on his body cam which conflicts the claim Chelan Sherifs make that their deputies are not equip[ed with body cams. But the Deputy did in fact ask and I graciously said yes please and thanks. They lack chain of custody for an impound claim. Further, the truck had a tire failure again due to low pressure and it had to be towed from Snoqualmie to Seattle that same night.  </p>
+          </ul>
+
+        </CardContent>
+      </Card>
     </div>
   );
 }
